b0b0548ec4ea6e2bec397d066d007239ad857cb3
Jason Aravanis <j.aravanis@icloud.com>
2020-10-10 17:47:10 +1100

    Project start, making calculator layout

---
+++ calculator.js
@@ -0,0 +1,6 @@
+// Set up overall calculator layout 
+const calculator = document.getElementById("calculator");
+const calculatorSize = 480;
+calculator.style.setProperty("width", calculatorSize + "px");
+calculator.style.setProperty("height", calculatorSize + "px");
+
