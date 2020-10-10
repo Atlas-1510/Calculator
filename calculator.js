403bdbb68f30e9b1ac909122fa12659c80a9c8d3
Jason Aravanis <j.aravanis@icloud.com>
2020-10-10 21:01:25 +1100

    Finished calculator layout

--- calculator.js
+++ calculator.js
@@ -1,6 +1,38 @@
+// ***** STYLING *****
 // Set up overall calculator layout 
 const calculator = document.getElementById("calculator");
 const calculatorSize = 480;
 calculator.style.setProperty("width", calculatorSize + "px");
 calculator.style.setProperty("height", calculatorSize + "px");
+
+// Button highlight effect when clicked
+const buttons = Array.from(document.querySelectorAll('.button'));
+buttons.forEach(button => button.addEventListener('mousedown', (button) => {
+    if (button.path[0].classList.contains("number")) {
+        button.path[0].classList.toggle("numberHighlight");
+    } else if (button.path[0].classList.contains("inputControl")) {
+        button.path[0].classList.toggle("inputHighlight");
+    } else if (button.path[0].classList.contains("operator")) {
+        button.path[0].classList.toggle("operatorHighlight");
+    }
+}));
+buttons.forEach(button => button.addEventListener('mouseup', (button) => {
+    if (button.path[0].classList.contains("number")) {
+        button.path[0].classList.toggle("numberHighlight");
+    } else if (button.path[0].classList.contains("inputControl")) {
+        button.path[0].classList.toggle("inputHighlight");
+    } else if (button.path[0].classList.contains("operator")) {
+        button.path[0].classList.toggle("operatorHighlight");
+    }
+}));
+
+// ***** FUNCTIONALITY *****
+
+function add(first, second) {
+    return first + second;
+}
+
+function subtract(first, second) {
+    return first - second;
+}
 
